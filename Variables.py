a6ec8bdb699850db544f1efe5583bdd1c2cdc455
abhishek adhar <abhishek@Ubuntu.myguest.virtualbox.org>
2023-05-15 14:19:35 +0530

    First Program

---
+++ Variables.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+print("My name is " + name + ", My age is "+ str(age) +" years, and my height is " + str(height) + " merters.")
